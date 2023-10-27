201b0dec21d204d0a10903ac8a7f74d4de7eb7bd
marwenlb <lbmarwan7@gmail.com>
2023-10-28 00:27:05 +0200

    ajout de quelque comentaire au code final version

--- src/main/java/HTMLTextPrinter.java
+++ src/main/java/HTMLTextPrinter.java
@@ -10,54 +10,64 @@ import java.util.Map;
  *
  * @author Labassi
  */
-class HTMLTextPrinter implements HTMLPrinter  {
-
+class HTMLTextPrinter implements HTMLPrinter {
     @Override
     public String generateHTML(Invoice invoice, Map<String, Play> plays) {
         int totalAmount = 0;
         int volumeCredits = 0;
+        // Create a StringBuilder for generating the HTML content
         StringBuilder html = new StringBuilder();
         
+        // Start creating the HTML document
         html.append("<html><head><title>Invoice</title></head><body>");
-
         html.append("<h1>Invoice</h1>");
         html.append("<p>Client: " + invoice.customer.getName() + "</p>");
-
+        // Create a table to display performance details
         html.append("<table border=\"1\">");
         html.append("<tr><th>Piece</th><th>Seats sold</th><th colspan='2'>Price</th></tr>");
 
+        // Iterate through the performances in the invoice
         for (Performance performance : invoice.performances) {
+            // Get the corresponding play for the performance
             Play play = plays.get(performance.playID);
 
             AmountCalculator amountCalculator = getAmountCalculator(play);
-
+            // Calculate the amount and volume credits for this performance
             int thisAmount = amountCalculator.calculateAmount(performance, play);
             int credits = amountCalculator.calculateVolumeCredits(performance, play);
-
             volumeCredits += credits;
 
+            // Add performance details to the HTML table
             html.append("<tr><td>" + play.name + "</td><td>" + performance.audience + "</td><td colspan='2'>$" + (thisAmount / 100.0) + "</td></tr>");
-            totalAmount += thisAmount;
+                        totalAmount += thisAmount;
         }
 
+        // Include total amount and volume credits in the HTML
         html.append("<tr><td colspan='2'><b>Total Owed</b></td><td >$" + (totalAmount / 100.0) + "</td></tr>");
         html.append("<tr><td colspan='2'><b>Volume Credits</b></td><td>" + volumeCredits + "</td></tr>");
 
+        // Close the HTML table and add payment information
         html.append("</table>");
-
         html.append("<p>Payment is required under 30 days. We can break your knees if you don't do so.</p>");
 
+        // Close the HTML document
         html.append("</body></html>");
+        
+        // Convert the StringBuilder result to a String and return it
         return html.toString();
     }
-        private AmountCalculator getAmountCalculator(Play play) {
+    
+    // Helper method to determine the appropriate AmountCalculator based on play type
+    private AmountCalculator getAmountCalculator(Play play) {
         if (play.getType() == Play.PlayType.COMEDY) {
             return new ComedyCalculator();
         } else if (play.getType() == Play.PlayType.TRAGEDY) {
             return new TragedyCalculator();
         } else {
+            // Throw an error for unknown play types
             throw new Error("Unknown type: " + play.getType());
         }
     }
 }
+
 
--- src/main/java/TextPrinter.java
+++ src/main/java/TextPrinter.java
@@ -16,44 +16,57 @@ import java.util.stream.Collectors;
 class TextPrinter implements Printer {
     @Override
     public String print(Invoice invoice, HashMap<String, Play> plays) {
-  Customer customer = invoice.getCustomer(); // Obtenez l'objet Customer depuis l'invoice
+        // Obtain the customer from the invoice
+        Customer customer = invoice.getCustomer();
         int totalAmount = 0;
         int volumeCredits = 0;
-        int loyaltyPoints = customer.getLoyaltyPoints(); // Obtenez les points de fidélité du client
+        
+        // Get the customer's loyalty points
+        int loyaltyPoints = customer.getLoyaltyPoints();
+        
+        // Create a StringBuilder to build the result
         StringBuilder result = new StringBuilder();
+        
+        // Create a currency formatter for US locale
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
-
-String customerName = invoice.customer.getName(); // Access the customer's name
+        String customerName = invoice.customer.getName();
         result.append(String.format("Statement for %s\n", customerName));
+        
+        // Iterate through the performances in the invoice
         for (Performance performance : invoice.performances) {
             Play play = plays.get(performance.playID);
-
             AmountCalculator amountCalculator = getAmountCalculator(play);
-
             int thisAmount = amountCalculator.calculateAmount(performance, play);
             int credits = amountCalculator.calculateVolumeCredits(performance, play);
-
             volumeCredits += credits;
 
+            // Add performance details to the result
             result.append(String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(thisAmount / 100), performance.audience));
             totalAmount += thisAmount;
         }
+
+        // Apply a loyalty points discount if the customer has more than 150 points
         if (loyaltyPoints > 150) {
             totalAmount -= 1500;
             loyaltyPoints -= 150;
         }
-        result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount / 100)));
+
+        // Add the total amount and volume credits to the result
+        result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount / 100));
         result.append(String.format("You earned %s credits\n", volumeCredits));
 
+        // Convert the StringBuilder result to a String and return it
         return result.toString();
     }
 
+    // Helper method to determine the appropriate AmountCalculator based on play type
     private AmountCalculator getAmountCalculator(Play play) {
         if (play.getType() == Play.PlayType.COMEDY) {
             return new ComedyCalculator();
         } else if (play.getType() == Play.PlayType.TRAGEDY) {
             return new TragedyCalculator();
         } else {
+            // Throw an error for unknown play types
             throw new Error("Unknown type: " + play.getType());
         }
     }
